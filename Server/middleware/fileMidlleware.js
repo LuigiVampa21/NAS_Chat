9ddddda314b7dc7ad260af77027ec426183ab940
TEISSIER Nathan <luigivampa12@protonmail.ch>
2022-10-23 08:25:28 +0200

    ntn

---
+++ Server/middleware/fileMidlleware.js
@@ -0,0 +1,24 @@
+const multer = require("multer");
+
+const MIME_TYPE_MAP = {
+  "image/png": "png",
+  "image/jpeg": "jpg",
+  "image/jpg": "jpg",
+};
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    const isValid = MIME_TYPE_MAP[file.mimetype];
+    let error = new Error("Invalid mime type");
+    if (isValid) {
+      error = null;
+    }
+    cb(error, "backend/images");
+  },
+  filename: (req, file, cb) => {
+    const name = file.originalname.toLowerCase().split(" ").join("-");
+    const ext = MIME_TYPE_MAP[file.mimetype];
+    cb(null, name + "-" + Date.now() + "." + ext);
+  },
+});
+
+exports.multerM = multer({ storage: storage }).single("image");
